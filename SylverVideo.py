2106db3b6da98cee5d72b97b0b2cc1f95737de34
20revsined <44682153+20revsined@users.noreply.github.com>
2020-07-13 13:09:44 -0400

    Update SylverVideo.py

--- SylverVideo.py
+++ SylverVideo.py
@@ -7,6 +7,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import webbrowser
 import requests
 
 class SylverVideoCall(SylverVideoCallTemplate):
@@ -14,17 +15,58 @@ class SylverVideoCall(SylverVideoCallTemplate):
    
     self.init_components(**properties)
   
-  def VideoCall(self):
-    url = "https://api.daily.co/v1/rooms"
-
-    payload = "{\"properties\":{\"autojoin\":false,\"enable_knocking\":true,\"enable_screenshare\":true,\"enable_chat\":true,\"start_video_off\":true,\"start_audio_off\":true,\"owner_only_broadcast\":false,\"eject_at_room_exp\":false,\"lang\":\"en\"},\"name\":\"hello\",\"privacy\":\"private\"}"
+  def IsPublic(self):
+    response = str(input("Do you want your video call to be public or private?"))
+    if response == "public":
+      response = "public"
+      return response
+    
+    elif response == "private":
+      response = "private"
+      return private
+  
+  def MaxParticipants(self):
+    maximum = int(input("Please enter the maximum number of people you would like to have in your video call."))
+    
+    if maximum > 200:
+      maximum = 200
+    
+    return maximum
+  
+  def ChatEnabled(self):
+    question = str(input("Would you like to enable a chat in your video call?"))
+    
+    if question == "yes":
+      chat = "true"
+    
+    elif question == "no":
+      chat = "false"
+     
+    return chat
+  
+  def VideoCallName(self):
+    name = str(input("Please name your video call."))
+  
+    return name
+  
+  def CreateVideoCall(self):
+    
+    MaxPeople = self.MaxParticipants()
+    IsChatEnabled = self.ChatEnabled()
+    CallName = self.VideoCallName()
+    public = self.IsPublic()
+    url = str("https://sylverteam.daily.co/" + CallName)
+    
+    information = "{\"properties\":{\"max_participants\":" + str(MaxPeople) + ",\"autojoin\":false,\"enable_knocking\":true,\"enable_screenshare\":true,\"enable_chat\":" + chat + ",\"start_video_off\":true,\"start_audio_off\":true,\"owner_only_broadcast\":false},\"privacy\:" + public + ",\"name\:" + CallName + "}"
+    
+    payload = information
     headers = {
     'content-type': "application/json",
     'authorization': "Bearer e144e63a4f56918684b38f42d5e9807eae1fc50f7bd67cb799691fb7f460087b"
     }
 
     response = requests.request("POST", url, data=payload, headers=headers)
-
-    print(response.text)
-
+    
+    webbrowser.open(url)
+    
 #source: https://docs.daily.co/reference#create-room
