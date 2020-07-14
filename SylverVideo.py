66361fc93e54d47d11fc5e6a36e9622ce90b11f2
20revsined <44682153+20revsined@users.noreply.github.com>
2020-07-14 09:00:25 -0400

    Update SylverVideo.py

--- SylverVideo.py
+++ SylverVideo.py
@@ -23,7 +23,7 @@ class SylverVideoCall(SylverVideoCallTemplate):
     
     elif response == "private":
       response = "private"
-      return private
+      return response
   
   def MaxParticipants(self):
     maximum = int(input("Please enter the maximum number of people you would like to have in your video call."))
@@ -55,18 +55,17 @@ class SylverVideoCall(SylverVideoCallTemplate):
     IsChatEnabled = self.ChatEnabled()
     CallName = self.VideoCallName()
     public = self.IsPublic()
-    url = str("https://sylverteam.daily.co/" + CallName)
+    url = "https://api.daily.co/v1/rooms/"
+    url2 = str("https://sylverteam.daily.co/" + CallName)
     
-    information = "{\"properties\":{\"max_participants\":" + str(MaxPeople) + ",\"autojoin\":false,\"enable_knocking\":true,\"enable_screenshare\":true,\"enable_chat\":" + chat + ",\"start_video_off\":true,\"start_audio_off\":true,\"owner_only_broadcast\":false},\"privacy\:" + public + ",\"name\:" + CallName + "}"
-    
-    payload = information
+    information = "{\"properties\":{\"max_participants\":%s,\"autojoin\":false,\"enable_knocking\":true,\"enable_screenshare\":true,\"enable_chat\":%s,\"start_video_off\":true,\"start_audio_off\":true,\"owner_only_broadcast\":false},\"name\":\"%s\",\"privacy\":\"%s\"}" % (str(MaxPeople), IsChatEnabled, CallName, public)
     headers = {
     'content-type': "application/json",
     'authorization': "Bearer e144e63a4f56918684b38f42d5e9807eae1fc50f7bd67cb799691fb7f460087b"
     }
 
-    response = requests.request("POST", url, data=payload, headers=headers)
+    response = requests.request("POST", url, data=information, headers=headers)
     
-    webbrowser.open(url)
+    webbrowser.open(url2)
     
-#source: https://docs.daily.co/reference#create-room
+#source: https://docs.daily.co/reference#create-room, https://stackoverflow.com/questions/38380462/syntaxerror-unexpected-token-o-in-json-at-position-1/38380728, https://docs.daily.co/reference#set-room-configuration
