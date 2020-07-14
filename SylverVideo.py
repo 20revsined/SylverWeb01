c4672129993f15b7036fd5c194bfb1840de287ee
20revsined <44682153+20revsined@users.noreply.github.com>
2020-07-14 13:33:24 -0400

    Update SylverVideo.py

--- SylverVideo.py
+++ SylverVideo.py
@@ -7,14 +7,53 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import webbrowser
-import requests
 
 class SylverVideoCall(SylverVideoCallTemplate):
   def __init__(self, **properties):
    
     self.init_components(**properties)
   
+
+  def IsPublic(self):
+      response = str(input("Do you want your video call to be public or private?\n"))
+      if response == "public":
+        response = "public"
+        
+      elif response == "private":
+        response = "private"
+      return response
+  
+  
+  def MaxParticipants(self):
+    maximum = int(input("Please enter the maximum number of people you would like to have in your video call.\n"))
+  
+    if maximum > 200:
+      maximum = 200
+        
+    return maximum
+    
+  def ChatEnabled(self):
+    question = str(input("Would you like to enable a chat in your video call?\n")) 
+    if question == "yes":
+      chat = "true"
+        
+    elif question == "no":
+      chat = "false"
+        
+    return chat
+  
+  def VideoCallName(self):
+    name = str(input("Please name your video call.\n"))
+    return name
+  
+  def MakeVideoCall(self):
+    maximum = self.MaxParticipants()
+    chat = self.ChatEnabled()
+    name = self.VideoCallName()
+    response = self.IsPublic()
+    anvil.server.call('CreateVideoCall', maximum, chat, name, response)
+    
+"""  
   def IsPublic(self):
     response = str(input("Do you want your video call to be public or private?"))
     if response == "public":
@@ -67,5 +106,6 @@ class SylverVideoCall(SylverVideoCallTemplate):
     response = requests.request("POST", url, data=information, headers=headers)
     
     webbrowser.open(url2)
-    
-#source: https://docs.daily.co/reference#create-room, https://stackoverflow.com/questions/38380462/syntaxerror-unexpected-token-o-in-json-at-position-1/38380728, https://docs.daily.co/reference#set-room-configuration
+"""    
+#sources: https://stackoverflow.com/questions/38380462/syntaxerror-unexpected-token-o-in-json-at-position-1/38380728, https://docs.daily.co/reference#set-room-configuration, https://docs.daily.co/reference#create-room,
+#https://anvil.works/docs/uplink/quickstart, https://anvil.works/docs/uplink, https://anvil.works/docs/server
